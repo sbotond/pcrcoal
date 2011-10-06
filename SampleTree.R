c9dbfa073ab29ee7c151084af6a6aa4625418d7a
Botond Sipos <sbotond@ebi.ac.uk>
2011-10-06 17:01:15 +0100

    Updated copyright header.

--- SampleTree.R
+++ SampleTree.R
@@ -1,7 +1,20 @@
-##  
-## Copyright 2010- Botond Sipos  
-## See the package description for licensing information.   
-## 
+#  Copyright (C) 2011 by Botond Sipos, European Bioinformatics Institute
+#  sbotond@ebi.ac.uk
+#
+#  This file is part of the pcrcoal software for coalescent simulations of PCR reactions.
+#
+#  pcrcoal is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  pcrcoal is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with pcrcoal. If not, see <http://www.gnu.org/licenses/>.
 
 .sampleTree<-function(this, size.traj, subsam){
 
